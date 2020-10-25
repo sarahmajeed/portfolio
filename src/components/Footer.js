f790fcf5d3015d9a07f413d8b4f5d597d2dbd54d
Sarahmdawood <sarahmajeed368@gmail.com>
2020-10-25 16:26:10 +0500

    Animating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,12 @@ function Footer() {
   return (
     <div className="footer">
       <p>
-        Designed and built by <a href="">{`<Sarah Majeed/>`}</a>
+        Designed and built by{" "}
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://github.com/Sarahmdawood"
+        >{`<Sarah Majeed/>`}</a>
       </p>
     </div>
   )
